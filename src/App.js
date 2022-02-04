4c78141591903e83683ceb5ced2dddc4ddd9eb09
Rishav Mahajan <mrishav016@gmail.com>
2022-02-04 23:35:26 +0530

    login template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Fragment } from "react";
+import Login from "./components/Home/Home";
+
+function App() {
+  return (
+    <Fragment>
+      <Login></Login>
+    </Fragment>
+  );
+}
+
+export default App;
